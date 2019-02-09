e11e8ea5707f54bebcc9de94ddefef3fba150026
Apl-serova <masya160485@mail.ru>
2019-02-09 21:39:28 +0300

    It's so cuuute

---
+++ Exercise9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Exercise9 {
+    public static void main (String [] args) {
+        Scanner s = new Scanner(System.in);
+        int[][] Mat = new int[3][3];
+        for (int j = 0; j<3; j++) {
+            System.out.println("Введите "+ (j+1) + " строку матрицы (3 столбца): ");
+            for (int i = 0; i<3; i++) {
+                Mat[j][i]= s.nextInt();
+            }
+        }
+        System.out.print("Элементы первой строки, кмноженные на 3: ");
+        for (int I=0; I<3;I++) {
+            System.out.print(Mat[0][I]*3+" ");
+        }
+    }
+}
